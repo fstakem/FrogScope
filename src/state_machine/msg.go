78844b7da145ff689597efcc19ecdc99ae48ad24
Fredrick Stakem <fstakem@fstakem-main.local>
2012-12-17 20:03:29 -0500

    Got unit testing working and trying to debug the json parser.

--- src/state_machine/msg.go
+++ src/state_machine/msg.go
@@ -1,7 +1,10 @@
 package state_machine
 
 import (
+	"fmt"
 	"encoding/json"
+	"bytes"
+	"strconv"
 )
 
 // Message enum type
@@ -13,36 +16,74 @@ const (
 )
 
 type LogMsg struct {
-	Ver int
+	Protocol string
+	Version int
 	SeqNum int
 	Scope string
 	Timestamp int64
-	Type MsgType
+	Transmitter string
+	Type string
 	Data map[string]string
 }
-
 
 func NewLogMsg(msg string) *LogMsg {
 	if len(msg) == 0 {
 		return nil
 	}
 	
-	log_msg := &LogMsg{}
+	var log_msg LogMsg
 	err := json.Unmarshal([]byte(msg), &log_msg)
 	
 	if err == nil {
 		return nil
 	}
 	
-	return log_msg
+	return &log_msg
+}
+
+func (this *LogMsg) Cmp(other *LogMsg) (equ bool) {
+	if this.Protocol != other.Protocol { 
+		fmt.Println(this.String())
+		fmt.Println(other.String())
+		return false 
+	} else if this.Version != other.Version {
+		return false 
+	} else if this.SeqNum != other.SeqNum {
+		return false
+	} else if this.Scope != other.Scope {
+		return false
+	} else if this.Timestamp != other.Timestamp {
+		return false
+	} else if this.Type != other.Type {
+		return false
+	}
+	
+	return true
+}
+
+func (this *LogMsg) String() (output string) {
+	var buffer bytes.Buffer
+	buffer.WriteString("Protocol: " + this.Protocol)
+	buffer.WriteString("  Version: " + strconv.Itoa(this.Version))
+	buffer.WriteString("  SeqNum: " + strconv.Itoa(this.SeqNum))
+	buffer.WriteString("  Scope: " + this.Scope)
+	buffer.WriteString("  Timestamp: " + strconv.FormatInt(this.Timestamp, 10))
+	buffer.WriteString("  Transmitter: " + this.Transmitter)
+	buffer.WriteString("  Type: " + this.Type)
+	
+	return buffer.String()
 }
  
+func ParseStateMachineMsg(data *map[string]string) *StateMachine {
 
-func ParseStateMachineMsg(msg string) *StateMachine {
+	for key, value := range *data {
+		fmt.Println("%s::%s",  key, value)
+	}
+	
 	return nil
 }
 
-func ParseStateTransitionMsg(msg string) *StateTransition {
+func ParseStateTransitionMsg(data *map[string]string) *StateTransition {
 	return nil
 }
 
--- src/state_machine/msg_test.go
+++ src/state_machine/msg_test.go
@@ -2,12 +2,54 @@ package state_machine
 
 import (
 	"io/ioutil"
+	"testing"
 )
 
-func readFile(filename string) {
-	data, err := ioutil.ReadFile(filename)
-    if err != nil { return nil, err }
+func readFile(filename string) (data string, err error) {
+	var raw_data []byte
+	raw_data, err = ioutil.ReadFile(filename)
+    if err != nil { return "", err }
     
-    return data, nil
+    return string(raw_data), nil
+}
+
+func TestNewLogMsg(t *testing.T) {
+	// Test data
+	test_log_msg := LogMsg{}
+	test_log_msg.Protocol = "frogscope"
+	test_log_msg.Version = 1
+	test_log_msg.SeqNum = 1
+	test_log_msg.Scope = "test"
+	test_log_msg.Timestamp = 123456789
+	test_log_msg.Transmitter = "main"
+	test_log_msg.Type = "state_transition"
+	test_log_msg.Data = make(map[string]string)
+	
+	// Import file
+	filename := "../../data/state_transition_event_1.json"
+	raw_data, err := readFile(filename)
+	if err != nil {
+		t.Log("Unable to open file.")
+		t.Fail()
+	}
+	
+	// Create object from imported file
+	t.Log("Data imported from the file:")
+	t.Log(raw_data)
+	log_msg := NewLogMsg(raw_data)
+	
+	// Test to make sure objects are the same
+	if !log_msg.Cmp(&test_log_msg) {
+		t.Log("The JSON data was not interpretted correctly.")
+		t.Fail()
+	}
+}
+
+func TestParseStateMachineMsg(t *testing.T) { 
+
+}
+
+func TestParseStateTransitionMsg(t *testing.T) {
+	
 }
 
